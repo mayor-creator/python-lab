243688f2a8eea9a4bc25faf47d525fa91bff9f2e
Mayor Creator <paulaboagye@outlook.com>
2026-07-22 13:26:58 -0400

    Add simple message exercise on assigning variables

---
+++ exercises/ex01/simple_message.py
@@ -0,0 +1,8 @@
+# assign a message to a variable 
+message = "Good morning, hope you're doing well?"
+print(message)
+
+# assign a message to a variable and change the value
+# to a new message
+message = "Bonjour ca va?"
+print(message)
