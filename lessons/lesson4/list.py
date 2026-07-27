c7f63d1e414dd8f2bc113f0f5d66a5cc2b344260
Mayor Creator <paulaboagye@outlook.com>
2026-07-27 14:47:45 -0400

    Add lesson on using insert(), pop() and remove() methods

--- lessons/lesson4/list.py
+++ lessons/lesson4/list.py
@@ -21,3 +21,25 @@ nested_list = ["A", "B", ["C", "D"]]
 # print letter D
 letter_d = nested_list[2][1]
 print(letter_d)
+
+# inserting a new item into a list using position
+fruits.insert(2, "Peach")
+print(fruits)
+
+# removing an item from a list with del statement
+del fruits[0]
+print(fruits)
+
+# removing an item from a list with pop method
+item_removed = fruits.pop()
+print(fruits)
+print(f"The last fruit I ate was {item_removed.title()} fruit.")
+
+# popping items from any position in a list
+first_fruit = fruits.pop(0)
+print(f"The first fruit of the day is {first_fruit} fruit.")
+
+# removing an item by value
+bad_fruit = "Strawberry"
+fruits.remove(bad_fruit)
+print(fruits)
