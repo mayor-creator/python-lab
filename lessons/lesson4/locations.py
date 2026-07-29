335dab767a328cc13751a8b6b39b65c475df300d
Mayor Creator <paulaboagye@outlook.com>
2026-07-29 09:23:38 -0400

    Add lesson on how to use sort, sorted and len methods

---
+++ lessons/lesson4/locations.py
@@ -0,0 +1,19 @@
+locations = [
+    "San Diego",
+    "Osaka",
+    "Houston",
+    "New Orleans",
+    "Paris",
+    "London",
+    "Tokyo",
+    "Barcelona",
+    "New York City",
+    "Kyoto",
+]
+print(f"Original location list: {locations}")
+
+location_sort = sorted(locations)
+print(f"Sort location list: {location_sort}")
+
+reverse_sort = sorted(locations, reverse=True)
+print(f"Reverse sort location list: {reverse_sort}")
