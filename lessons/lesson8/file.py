6b257c1a4c974ea5d9648f86bc7fd7884fe52bc2
Mayor Creator <paulaboagye@outlook.com>
2026-08-04 06:10:47 -0400

    Add lesson on reading and writing to file

---
+++ lessons/lesson8/file.py
@@ -0,0 +1,12 @@
+# with statement creates a runtime context that allows you
+# execute a code block under the control of a context manager
+
+# reading a file
+with open("python-lab/lessons/lesson8/my_file.txt", mode="r") as file:
+    content = file.read()
+
+print(content)
+
+# writing to a file
+with open("python-lab/lessons/lesson8/new_file.txt", mode="w") as file:
+    file.write("Welcome to Paris.\nYou need to visit Louvre Museum before you leave.")
