05d68468024c5bfd44077fefdc6907422f63d539
Mayor Creator <paulaboagye@outlook.com>
2026-08-31 21:45:42 -0400

    Initial commit

---
+++ project/project7/formula_one.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import fastf1
+
+cache_dir = Path("./cache")
+cache_dir.mkdir(parents=True, exist_ok=True)
+fastf1.Cache.enable_cache(str(cache_dir))
+
+session = fastf1.get_session(2026, "Netherlands", "Q")
+session.load()
+
+print(f"Session Name: {session.name}")
+print(f"Grand Prix: {session.event['EventName']}")
+
+print("***** Results of the top Ten Qualifiers *****")
+print(session.results.iloc[0:10].loc[:, ["Abbreviation", "Q3"]])
