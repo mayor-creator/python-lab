345f2c847f70ae58dfb19cf582e978b53b0e3469
Mayor Creator <paulaboagye@outlook.com>
2026-09-02 22:13:45 -0400

    Add lesson on Flask and Decorators

---
+++ lessons/lesson20/decorator.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+# a decorator wraps a function, modifying its behavior
+
+
+def decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called")
+        func()
+        print("Something is happening after the function is called")
+
+    return wrapper
+
+
+def say_whee():
+    print("Whee!")
+
+
+say_whee = decorator(say_whee)
+say_whee()
+
+
+def not_during_the_night(func):
+    def wrapper():
+        if 7 <= datetime.now().hour < 22:
+            func()
+        else:
+            pass
+
+    return wrapper
+
+
+def say_hi():
+    print("Hi!!!")
+
+
+say_hi = not_during_the_night(say_hi)
+say_hi()
